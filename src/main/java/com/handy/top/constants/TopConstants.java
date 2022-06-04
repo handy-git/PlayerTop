ef47798ebbfbfc0f1929583d3da6af55fd2fda72
handy <13546606929@163.com>
2022-06-04 18:21:32 +0800

    修改更新url

--- src/main/java/com/handy/top/constants/TopConstants.java
+++ src/main/java/com/handy/top/constants/TopConstants.java
@@ -10,6 +10,6 @@ public abstract class TopConstants {
     /**
      * 更新记录url
      */
-    public final static String PLUGIN_VERSION_URL = "https://api.github.com/repos/handy-git/PlayerTaskVersions/releases/latest";
+    public final static String PLUGIN_VERSION_URL = "https://api.github.com/repos/handy-git/PlayerTop/releases/latest";
 
 }
